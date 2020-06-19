6112b6c109d95f1b251dccf76114005db2ca323a
WaveF <wavef@qq.com>
2020-06-20 00:48:39 +0800

    fixed some text did not change to mocked text

--- libs/axmock.js
+++ libs/axmock.js
@@ -1,6 +1,6 @@
 /**
  * 
- * AxMock v0.1.2
+ * AxMock v0.1.4
  * 
  * Author: WaveF
  * QQ: 298010937
@@ -22,85 +22,87 @@
     }, 100);
 
     function main() {
+        console.clear();
+        
+        let css = [
+            '.typed-cursor { display:none; }'
+        ].join('');
+        $('head').append('<style>' + css + '</style>');
+
         let axTextEls = $axure('*').$().find('span');
         axTextEls.each((i, k) => {
-            mockText( $(k), '((日期))', Mock.mock('@datetime("yyyy年MM月dd日")') );
-            mockText( $(k), '((时间))', Mock.mock('@datetime("HH:mm:ss")') );
-            mockText( $(k), '((日期时间))', Mock.mock('@datetime("yyyy年MM月dd日 HH:mm:ss")') );
-            
-            mockText( $(k), '((数字))', Mock.mock({ "num|1-999": 1 }).num );
-            mockText( $(k), '((数字#1))', Mock.mock({ "num|1-9": 1 }).num );
-            mockText( $(k), '((数字#2))', Mock.mock({ "num|10-99": 10 }).num );
-            mockText( $(k), '((数字#3))', Mock.mock({ "num|100-999": 100 }).num );
-            mockText( $(k), '((数字#4))', Mock.mock({ "num|1000-9999": 1000 }).num );
-            mockText( $(k), '((数字#5))', Mock.mock({ "num|10000-99999": 10000 }).num );
-            mockText( $(k), '((数字#中))', Mock.mock('@cword("零一二三四五六七八九十")') );
-            
-            mockText( $(k), '((字母))', Mock.mock('@string("upper", 1, 10)') );
-            mockText( $(k), '((字母#1))', Mock.mock('@string("upper", 1, 1)') );
-            mockText( $(k), '((字母#2))', Mock.mock('@string("upper", 2, 2)') );
-            mockText( $(k), '((字母#3))', Mock.mock('@string("upper", 3, 3)') );
-            mockText( $(k), '((字母#4))', Mock.mock('@string("upper", 4, 4)') );
-            mockText( $(k), '((字母#5))', Mock.mock('@string("upper", 5, 5)') );
+            let currentText = $(k).text();
+            let updateText = currentText;
 
-            mockText( $(k), '((验证码))', Math.random().toString(36).slice(2,6).toUpperCase() );
-            mockText( $(k), '((验证码#4))', Math.random().toString(36).slice(2,6).toUpperCase() );
-            mockText( $(k), '((验证码#5))', Math.random().toString(36).slice(2,7).toUpperCase() );
-            mockText( $(k), '((验证码#6))', Math.random().toString(36).slice(2,8).toUpperCase() );
-
-            mockText( $(k), '((姓名))', Mock.mock('@cname()') );
-            mockText( $(k), '((姓氏))', Mock.mock('@cfirst()') );
-            mockText( $(k), '((名字))', Mock.mock('@clast()') );
-            mockText( $(k), '((性别))', Math.random()>.5?'男':'女');
-            mockText( $(k), '((年龄))', Mock.mock({ "num|18-50": 18 }).num + '岁' );
-            mockText( $(k), '((电邮))', Mock.mock('@email()') );
-
-            mockText( $(k), '((省份))', Mock.mock('@province()') );
-            mockText( $(k), '((城市))', Mock.mock('@city()') );
-            mockText( $(k), '((省市))', Mock.mock('@city(true)') );
-            mockText( $(k), '((市区))', Mock.mock('@county(true)') );
-
-            mockColor( $(k), Mock.mock('@color()') );
-            
-            mockText( $(k), '((标题))', Mock.mock('@ctitle()') );
-            mockText( $(k), '((单句))', (Mock.mock('@csentence()')).split('。').join('') );
-            mockText( $(k), '((段落))', Mock.mock('@cparagraph()') );
-        });
-    }
-
-    function mockText(target, marker, mocked) {
-        if ($(target).hasClass('mock-text')) return;
-
-        var currentText = $(target).text();
-        // console.log(getLength(marker));
-        
-        if (currentText.indexOf(marker) > -1) {
-            let textRoot = $(target).parents('.text').parent();
+            let textRoot = $(k).parents('.text').parent();
             let textId = textRoot.attr('id');
-            var newText = currentText.split(marker).join(mocked);
-            // $(target).text(newText);
+            let mockId = `ax${i}_mock-text_${textId}`;
+            $(k).attr('id', mockId).addClass('mock-text');
 
+            updateText = mockText( updateText, '((日期))', Mock.mock('@datetime("yyyy年MM月dd日")') );
+            updateText = mockText( updateText, '((时间))', Mock.mock('@datetime("HH:mm:ss")') );
+            updateText = mockText( updateText, '((日期时间))', Mock.mock('@datetime("yyyy年MM月dd日 HH:mm:ss")') );
+            updateText = mockText( updateText, '((数字))', Mock.mock({ "num|1-999": 1 }).num );
+            updateText = mockText( updateText, '((数字#1))', Mock.mock({ "num|1-9": 1 }).num );
+            updateText = mockText( updateText, '((数字#2))', Mock.mock({ "num|10-99": 10 }).num );
+            updateText = mockText( updateText, '((数字#3))', Mock.mock({ "num|100-999": 100 }).num );
+            updateText = mockText( updateText, '((数字#4))', Mock.mock({ "num|1000-9999": 1000 }).num );
+            updateText = mockText( updateText, '((数字#5))', Mock.mock({ "num|10000-99999": 10000 }).num );
+            updateText = mockText( updateText, '((数字#中))', Mock.mock('@cword("零一二三四五六七八九十")') );
+            updateText = mockText( updateText, '((字母))', Mock.mock('@string("upper", 1, 10)') );
+            updateText = mockText( updateText, '((字母#1))', Mock.mock('@string("upper", 1, 1)') );
+            updateText = mockText( updateText, '((字母#2))', Mock.mock('@string("upper", 2, 2)') );
+            updateText = mockText( updateText, '((字母#3))', Mock.mock('@string("upper", 3, 3)') );
+            updateText = mockText( updateText, '((字母#4))', Mock.mock('@string("upper", 4, 4)') );
+            updateText = mockText( updateText, '((字母#5))', Mock.mock('@string("upper", 5, 5)') );
+            updateText = mockText( updateText, '((验证码))', Math.random().toString(36).slice(2,6).toUpperCase() );
+            updateText = mockText( updateText, '((验证码#4))', Math.random().toString(36).slice(2,6).toUpperCase() );
+            updateText = mockText( updateText, '((验证码#5))', Math.random().toString(36).slice(2,7).toUpperCase() );
+            updateText = mockText( updateText, '((验证码#6))', Math.random().toString(36).slice(2,8).toUpperCase() );
+            updateText = mockText( updateText, '((姓名))', Mock.mock('@cname()') );
+            updateText = mockText( updateText, '((姓氏))', Mock.mock('@cfirst()') );
+            updateText = mockText( updateText, '((名字))', Mock.mock('@clast()') );
+            updateText = mockText( updateText, '((性别))', Math.random()>.5?'男':'女');
+            updateText = mockText( updateText, '((年龄))', Mock.mock({ "num|18-50": 18 }).num + '岁' );
+            updateText = mockText( updateText, '((电邮))', Mock.mock('@email()') );
+            updateText = mockText( updateText, '((省份))', Mock.mock('@province()') );
+            updateText = mockText( updateText, '((城市))', Mock.mock('@city()') );
+            updateText = mockText( updateText, '((省市))', Mock.mock('@city(true)') );
+            updateText = mockText( updateText, '((市区))', Mock.mock('@county(true)') );
+            updateText = mockText( updateText, '((标题))', Mock.mock('@ctitle()') );
+            updateText = mockText( updateText, '((单句))', (Mock.mock('@csentence()')).split('。').join('') );
+            updateText = mockText( updateText, '((段落))', Mock.mock('@cparagraph()') );
+            updateText = mockColor($(k), updateText);
 
-            $(target).attr('id', `mock-text-${textId}`).addClass('mock-text');
-            
-            console.log($(target));
-            let typed = new Typed(`#mock-text-${textId}`, {
-                strings: [currentText, newText],
+            // $(k).text(currentText);
+            new Typed(`#${mockId}`, {
+                strings: [currentText, updateText || '&nbsp;'],
                 typeSpeed: 30
             });
+            
+        });
 
-            $('.typed-cursor').hide();
-        }
     }
 
-    function mockColor(target, color) {
-        var marker = '((颜色))';
-        var currentText = $(target).text();
-        if (currentText.indexOf(marker)==-1) return;
+    function mockText(oldText, marker, mocked) {
+        if (oldText.indexOf(marker) > -1) {
+            var newText = oldText.split(marker).join(mocked);
+            return newText;
+        }
+        return oldText;
+    }
 
-        var root = $(target).parents('.text').parent();
-        root.css('background', color).find('img').hide();
-        $(target).text(currentText.split(marker).join(color));
+    function mockColor(target, oldText) {
+        let marker = '((颜色))';
+        let color = Mock.mock('@color()');
+        if (oldText.indexOf(marker)>-1) {
+            let newText = oldText.split(marker).join(color)
+            let root = $(target).parents('.text').parent();
+            root.css('background', color).find('img').hide();
+            return newText;
+        } else {
+            return oldText;
+        }
     }
 
     function getLength(str) {
