03f546eb94fbc6b43a957ae6c3dde84720adebb1
WaveF <wavef@qq.com>
2023-10-22 22:06:55 +0800

    use fixed file name

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ const { defineConfig } = require('vite')
 const MODULE_NAME = 'axlib'
 module.exports = defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, 'lib/main.js'),
       name: MODULE_NAME,
-      fileName: MODULE_NAME,
-      formats: ['es', 'cjs', 'umd', 'iife'],
+      formats: ['iife'],
+      fileName: format => `axlib-v3.min.js`,
+      // formats: ['es', 'cjs', 'umd', 'iife'],
       // fileName: (format) => `${MODULE_NAME}.${format}.js`
     }
   }
